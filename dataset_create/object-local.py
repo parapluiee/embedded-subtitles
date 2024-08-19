985cc6dad14b740686aaeb7e74c56d71bb45f926
parapluiee <eli.i.stafford27@gmail.com>
2024-08-19 03:50:04 +0000

    using empty resnet model

--- dataset_create/object-local.py
+++ dataset_create/object-local.py
@@ -1,6 +1,7 @@
 import torch
 from torchvision import transforms
 from torchvision.models.resnet import ResNet18_Weights
+import torchvision
 from torch import nn
 from loaddataset import data_dict
 from dataset_obj import CustomImageDataset
@@ -48,7 +49,9 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs):
             i+=1
 
         # Calculate the train loss and accuracy
-        train_loss = running_loss / len(train_dataset)
+        print("train loss")
+        train_loss = running_loss / len(train_loader)
+
         #train_acc = running_corrects.double() / len(train_dataset)
         train_acc = 0
         # Set the model to evaluation mode
@@ -59,11 +62,15 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs):
         #running_corrects = 0
 
         # Iterate over the batches of the validation loader
+        """
         with torch.no_grad():
+            i = 0
             for inputs, labels in val_loader:
+                if (i % 4 == 0):
+                    print("Validation: ", i)
                 # Move the inputs and labels to the device
-                inputs = inputs.to(device)
-                labels = labels.to(device)
+                #inputs = inputs.to(device)
+                #labels = labels.to(device)
 
                 # Forward pass
                 outputs = model(inputs)
@@ -74,18 +81,20 @@ def train(model, train_loader, val_loader, criterion, optimizer, num_epochs):
                 #running_corrects += torch.sum(preds == labels.data)
 
         # Calculate the validation loss and accuracy
-        val_loss = running_loss / len(val_dataset)
+        val_loss = running_loss / len(val_loader)
         #val_acc = running_corrects.double() / len(val_dataset)
         val_acc = 0
+        """
         # Print the epoch results
-        print('Epoch [{}/{}], train loss: {:.4f}, train acc: {:.4f}, val loss: {:.4f}, val acc: {:.4f}'
-              .format(epoch+1, num_epochs, train_loss, train_acc, val_loss, val_acc))
+        print('Epoch [{}/{}], train loss: {:.4f}, train acc: undef, val loss: undef, val acc: undef'
+              .format(epoch+1, num_epochs, train_loss))
+        #need to add accuracy 
+
 
 def main():
     print('Loading Model')
-    model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', weights=ResNet18_Weights.DEFAULT)
-    for param in model.parameters():
-        param.requires_grad = False
+    #model = torch.hub.load('pytorch/vision:v0.10.0', 'resnet18', weights=ResNet18_Weights.DEFAULT)
+    model = torchvision.models.resnet18()
     #currently replaces final layer
     model.fc = nn.Sequential(nn.Linear(model.fc.in_features, 4))
     print('Model Loaded')
@@ -106,6 +115,8 @@ def main():
 
     optimizer = torch.optim.SGD(model.fc.parameters(), lr=0.01, momentum=.9)
     criterion = torch.nn.CrossEntropyLoss()
+    gpu = torch.cuda.is_available()
+    print("GPU Available: ", gpu)  
     print('\nBegin Training')
     train(model, dl_train, dl_valid, criterion, optimizer, num_epochs=10)
 main()
