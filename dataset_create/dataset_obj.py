7c249f879f1fea14fd854f6f74e7d59229e67065
parapluiee <eli.i.stafford27@gmail.com>
2024-12-02 17:22:30 +0100

    refactored

--- dataset_create/dataset_obj.py
+++ dataset_create/dataset_obj.py
@@ -4,20 +4,24 @@ import pandas as pd
 from torchvision.io import read_image
 from torch.utils.data import Dataset
 import torch
+import numpy as np
 class CustomImageDataset(Dataset):
-    def __init__(self, labels, img_dir, transform=None):
-        self.img_labels = labels
-        self.img_dir = img_dir
+    def __init__(self, input_path, shape_path, labels_path, labels_shape_path, transform=None):
+        self.imgs = np.reshape(np.load(input_path), np.load(shape_path))
+        self.labels = np.reshape(np.load(labels_path), np.load(labels_shape_path))
         self.transform = transform
 
     def __len__(self):
-        return len(self.img_labels)
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.index[idx])
-        image = Image.open(img_path)
-        label = self.img_labels.iloc[idx]
+        #image = Image.fromarray(self.imgs[idx])
+        image = self.imgs[idx]
+        label = self.labels[idx]
+        image = torch.tensor(image).float()
+        image = torch.unsqueeze(image, 0)
+        label = torch.tensor(label).float()
+        label = torch.unsqueeze(label, 0)
         if self.transform:
-            ten_image = self.transform(image)
-        label = torch.tensor(label.values.astype(float))
-        return ten_image, label
+            image = self.transform(image)
+        return image, label
